b02af97ad8d75c217ecd12c5e19e04f2a21db977
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-03-01 16:21:38 -0300

    feat: renderize solar system planets
    requirement 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,19 @@
 import { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {
+          planets.map((planet) => {
+            const { name, image } = planet;
+            return <PlanetCard planetName={ name } planetImage={ image } key={ name } />;
+          })
+        }
       </div>
     );
   }

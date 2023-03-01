f5f3158212622867804dadbb592a173c0605a194
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-03-01 18:06:26 -0300

    style: body and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import { Component } from 'react';
+import logo from '../images/logo-sistema-solar.png';
+import './Header.css';
 
 class Header extends Component {
   render() {
     return (
       <header>
         <h1>Sistema Solar</h1>
+        <img src={ logo } alt="Logo do Sistema Solar" />
       </header>
     );
   }

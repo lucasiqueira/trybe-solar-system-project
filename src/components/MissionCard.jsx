1df45370bd8da761bab9e5820cbb7502a7d486fe
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-03-01 21:23:49 -0300

    style: missions

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,15 +1,34 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
+import './MissionCard.css';
+import divisoria from '../images/divisoria.png';
+import calendar from '../images/calendar.png';
+import local from '../images/local.png';
+import destinationFlag from '../images/destinationFlag.png';
 
 class MissionCard extends Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <p data-testid="mission-name">{ name }</p>
-        <p data-testid="mission-year">{ year }</p>
-        <p data-testid="mission-country">{ country }</p>
-        <p data-testid="mission-destination">{ destination }</p>
+      <div data-testid="mission-card" className="mission-card">
+        <div>
+          <p data-testid="mission-name" className="mission-name">{ name }</p>
+        </div>
+        <img src={ divisoria } alt="" className="divisoria" />
+        <div className="mission-data">
+          <div className="mission-year-country">
+            <img src={ calendar } alt="" className="icon" />
+            <p data-testid="mission-year" className="mission-year">{ year }</p>
+            <img src={ local } alt="" className="icon local-pin" />
+            <p data-testid="mission-country" className="mission-country">{ country }</p>
+          </div>
+          <div className="destination-div">
+            <img src={ destinationFlag } alt="" className="icon" />
+            <p data-testid="mission-destination" className="mission-destination">
+              { destination }
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -2,26 +2,29 @@ import { Component } from 'react';
 import Title from './Title';
 import missions from '../data/missions';
 import MissionCard from './MissionCard';
+import './Missions.css';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div data-testid="missions" className="missions">
         <Title headline="Missões" />
-        {
-          missions.map((mis) => {
-            const { name, year, country, destination } = mis;
-            return (
-              <MissionCard
-                name={ name }
-                year={ year }
-                country={ country }
-                destination={ destination }
-                key={ name }
-              />
-            );
-          })
-        }
+        <div className="missions-container">
+          {
+            missions.map((mis) => {
+              const { name, year, country, destination } = mis;
+              return (
+                <MissionCard
+                  name={ name }
+                  year={ year }
+                  country={ country }
+                  destination={ destination }
+                  key={ name }
+                />
+              );
+            })
+          }
+        </div>
       </div>
     );
   }
